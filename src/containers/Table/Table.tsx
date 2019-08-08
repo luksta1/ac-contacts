717e2b9638ca2b88f141f5f6cf51affffdfb03d0
Luke Pura <lukepura@gmail.com>
2019-08-07 22:52:10 -0500

    refactor TableHeader & TableRow folder structure; set up basis for Table

--- src/containers/Table/Table.tsx
+++ src/containers/Table/Table.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import TableHeader from '../../components/TableHeader';
+import TableRow from '../../components/TableRow';
 
-const Table: React.FC = () => {
-  return (
-    <></>
-  );
+class Table extends React.Component<Object, Object> {
+    render(): React.ReactNode {
+        return (
+            <main>
+                <table>
+                    <tbody>
+                        <TableHeader />
+                        <TableRow />
+                    </tbody>
+                </table>
+            </main>
+          );
+    }
 }
 export default Table;
